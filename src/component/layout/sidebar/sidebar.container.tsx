cdaaecff505615474fa11dd7ff873f333001b17a
nostrss <jintagi@gmail.com>
2022-12-18 03:25:05 +0900

    close #5

--- src/component/layout/sidebar/sidebar.container.tsx
+++ src/component/layout/sidebar/sidebar.container.tsx
@@ -1,13 +1,19 @@
 import SideBarUI from './sidebar.presenter';
 import { firebaseDb } from 'App';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { loadMemoList } from 'common/util';
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { INewTite } from './sidebar.type';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMemoList } from 'redux/createSlice/createSlice';
 
 export default function SideBar() {
   const [memoTitle, setMemoTitle] = useState('');
+
+  const stateData = useSelector((state: any) => state.nodePad);
+  const dispatch = useDispatch();
+
   const { mutate: createTitle, isLoading: isMutating } = useMutation(
     (newTitle: INewTite) => {
       return addDoc(collection(firebaseDb, 'no-sign-in'), newTitle);
@@ -22,6 +28,13 @@ export default function SideBar() {
     queryKey: ['fetchdata'],
     queryFn: async () => loadMemoList(firebaseDb, 'no-sign-in'),
   });
+
+  useEffect(() => {
+    dispatch(getMemoList(JSON.stringify(memoLists)));
+  }, [memoLists]);
+
+  console.log(stateData);
+  console.log(memoLists);
 
   const onSubmitMemoTitle = async (event: React.SyntheticEvent) => {
     event.preventDefault();
