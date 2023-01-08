205724af46ded0f55591c5571552eb45582e6df7
nostrss <jintagi@gmail.com>
2023-01-08 11:36:41 +0900

    close #9

--- src/pages/memo/memo.container.tsx
+++ src/pages/memo/memo.container.tsx
@@ -3,15 +3,20 @@ import { firebaseDb } from 'App';
 import { loadMemo } from 'common/util';
 import { useParams } from 'react-router-dom';
 import MemoUI from './memo.presenter';
+import * as DOMPurify from 'dompurify';
 
 export default function Memo() {
   const { id: pageId } = useParams();
-  console.log('pageId', pageId);
+  const sampleData = '<li>hi</li>';
+
+  const cleanData = DOMPurify.sanitize(sampleData);
 
   const { data: memoData, isLoading: isFetching } = useQuery({
     queryKey: ['fetchdata', pageId],
     queryFn: async () => loadMemo(firebaseDb, 'no-sign-in', pageId as string),
   });
 
-  return <MemoUI memoData={memoData} isFetching={isFetching} />;
+  return (
+    <MemoUI memoData={memoData} isFetching={isFetching} cleanData={cleanData} />
+  );
 }
